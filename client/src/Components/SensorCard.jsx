9fbd23d440cc451cc18d79493fc99f84b7d5b4ca
DuyITLOR <153221271+DuyITLOR@users.noreply.github.com>
2025-09-20 00:35:16 +0700

    Complete the dashboard

--- client/src/Components/SensorCard.jsx
+++ client/src/Components/SensorCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SensorCard = ({label, Icon, data,  alertText}) => {
     return (
-        <div className="rounded-2xl border bg-green-50 border-green-200 p-6 w-full max-w-sm shadow-sm">
+        <div className="rounded-2xl border bg-white border-green-200 p-6 w-full max-w-sm shadow-sm">
             <div className="flex items-center justify-between mb-4">
                 <h2 className="text-lg font-medium text-green-900">{label}</h2>
                 <Icon size = {25}/>
